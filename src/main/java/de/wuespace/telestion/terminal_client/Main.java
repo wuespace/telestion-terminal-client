fc0b161121adede52dd3e3b863fa509d89262a68
Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>
2021-06-24 14:42:14 +0200

    feat: Add simple line reader

--- src/main/java/de/wuespace/telestion/terminal_client/Main.java
+++ src/main/java/de/wuespace/telestion/terminal_client/Main.java
@@ -1,7 +1,24 @@
 package de.wuespace.telestion.terminal_client;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        String input;
+        boolean loop = true;
+        Scanner scanner = new Scanner(System.in);
+
+        while (loop) {
+            System.out.print("> ");
+            input = scanner.nextLine();
+            switch (input) {
+                case "/exit", "/quit" -> loop = false;
+                default -> System.out.println(input);
+            }
+        }
+
+        System.out.println("Quit");
     }
 }
